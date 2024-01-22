3cb16fdbfd5d221bfe80ca3ecd63d47e5e86f6a5
moolar1989 <156556143+moolar1989@users.noreply.github.com>
2024-01-21 18:00:22 -0600

    start

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ let userFavorite = prompt("What is your favorite thing?");
 // 7. Use console.log() to print both variables to the console
 console.log("myNumber:", myNumber);
 console.log("userFavorite:", userFavorite);
+
+// 8. Create a new variable starting at a number of your choice
+let startingNumber = 3; // You can choose any starting number
+
+// 9. Create a for loop that iterates 5 times
+for (let i = 0; i < 5; i++) {
+    // If the variable from step 8 is odd, print it
+    if (startingNumber % 2 !== 0) {
+        console.log("Odd Number:", startingNumber);
+    }
+
+    // Regardless of odd or even, increment the variable
+    startingNumber++;
+}
