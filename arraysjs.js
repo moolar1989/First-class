85a6c97d3988e63d462fcf924a27675451f2a3f8
moolar1989 <156556143+moolar1989@users.noreply.github.com>
2024-01-27 21:52:12 -0600

    start

--- arraysjs.js
+++ arraysjs.js
@@ -1,15 +1,13 @@
 // arrays.js
 
-// 3. Create a new array
+
 const myArray = [1, 2, 3, 4];
 
-// 4. Create a new function expression
 const addOne = function (num) {
     num += 1;
     console.log(num);
 };
 
-// 5. Create a new function with two parameters
 const processNumber = function (func, number) {
     if (number % 2 === 0) {
         func(number);
@@ -18,8 +16,7 @@ const processNumber = function (func, number) {
     }
 };
 
-// 6. Create a for-in loop
 for (const index in myArray) {
-    // 7. Call the function from step 5 in each iteration
+   
     processNumber(addOne, myArray[index]);
 }
