f15c2712d4611fb1a66a2790a90f64eb17b7f94a
moolar1989 <156556143+moolar1989@users.noreply.github.com>
2024-02-25 08:01:34 -0600

    start

---
+++ unit 2/lib_example.js
@@ -0,0 +1,10 @@
+// lib_example.js
+class ExampleClass {
+    constructor(name) {
+        this.name = name;
+    }
+
+    logName() {
+        console.log("Name:", this.name);
+    }
+}
---
+++ unit 2/objects.js
@@ -0,0 +1,5 @@
+let obj1 = new ExampleClass("Object 1");
+let obj2 = new ExampleClass("Object 2");
+
+obj1.logName();
+obj2.logName();
